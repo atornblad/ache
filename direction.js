f98781ed45f8c5af73315a5700a23cc2bf9b5b4f
Anders Tornblad, web developer <anders.tornblad@gmail.com>
2014-12-12 15:15:33 +0100

    Better hue2rgb, nicer song

--- direction.js
+++ direction.js
@@ -54,21 +54,20 @@ var polyPoly = function(polys, ctx) {
 };
 
 // http://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+var hue2rgb = function(p, q, t){
+    if(t < 0) t += 1;
+    if(t > 1) t -= 1;
+    if(t < 1/6) return p + (q - p) * 6 * t;
+    if(t < 1/2) return q;
+    if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    return p;
+}
 var hslToRgb = function (h, s, l){
     var r, g, b;
 
-    if(s == 0){
+    if (s == 0) {
         r = g = b = l; // achromatic
-    }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
+    } else {
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
         r = hue2rgb(p, q, h + 1/3);
@@ -649,6 +648,7 @@ var demo = {
         context.stroke();
         
         // Finally: The mandatory text scroller
+        /*
         var text = "Welcome to the \"Ache\" demo by Anders \"LBRTW\" Tornblad!";
         var textFill = context.createLinearGradient(0, 440, 0, 500);
         textFill.addColorStop(0.1, "#33ddff");
@@ -670,6 +670,7 @@ var demo = {
         var textX = 700 - 190 * floatPattern;
         context.fillText(text, textX, 442);
         context.strokeText(text, textX, 442);
+        */
         
         // When the drums start beating, do this pretty thing...
         if (pattern == 13 && beat >= 28) {
